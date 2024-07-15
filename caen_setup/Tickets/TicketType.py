2d3bbc2e2110e1367fe04391790af4ed75d73695
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2024-07-15 13:16:03 +0700

    Fix to issue #19. Also check out prev commit.

--- caen_setup/Tickets/TicketType.py
+++ caen_setup/Tickets/TicketType.py
@@ -1,14 +1,12 @@
 from enum import Enum
 
 from caen_setup.Tickets import Tickets
-from caen_setup.Tickets.TicketInfo import Ticket_Type_info
 
 
 class TicketType(Enum):
     SetVoltage = Tickets.SetVoltage_Ticket
     Down = Tickets.Down_Ticket
     GetParams = Tickets.GetParams_Ticket
-    StepByStepRUp = Tickets.StepByStepRUp_Ticket
 
     @classmethod
     def names(cls) -> set[str]:
