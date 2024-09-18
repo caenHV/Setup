58dbded24428361a5814c284bbfacfd9bbe6ee98
nikita-p <nikita.petrov.1997@gmail.com>
2024-09-18 15:24:15 +0700

    Update fake board
    * Add gaussian noise to fake parameters
    * Add IMonH fake values

--- caen_setup/Setup/board.py
+++ caen_setup/Setup/board.py
@@ -30,7 +30,17 @@ class FakeBoard:
         for ch in channels:
             res_dict[ch] = {p: 0 for p in parameters}
             if "VMon" in parameters:
-                res_dict[ch]["VMon"] = FakeBoard.board_state.get("VMon", {}).get(handler, {}).get(ch, 0)
+                res_dict[ch]["VMon"] = (
+                    FakeBoard.board_state.get("VMon", {})
+                    .get(handler, {})
+                    .get(ch * random.gauss(1, 0.02), 0)
+                )
+            if "IMonH" in parameters:
+                res_dict[ch]["IMonH"] = (
+                    FakeBoard.board_state.get("VMon", {})
+                    .get(handler, {})
+                    .get((ch / 1000) * random.gauss(1, 0.02), 0)
+                )
         return res_dict
 
     @staticmethod
